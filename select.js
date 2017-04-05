852c914a3263a889dfaa331373ecc18f7ec9479f
Kanojah <kanoa.hi@gmail.com>
2017-04-05 10:49:23 -1000

    pau

--- select.js
+++ select.js
@@ -4,5 +4,25 @@ function select (array) {
   if (Array.isArray(array) !== true) {
     return false;
   }
+  let curr;
+  let next;
+  for (let i = 0; i < array.length - 1; i++) {
+    if (typeof (array[i]) !== 'number' || typeof (array[array.length - 1]) !== 'number') {
+      throw new Error('not all numbers');
+    }
+    curr = i;
+    for (let y = i + 1; y < array.length; y++) {
+      if (array[y] < array[curr]) {
+        curr = y;
+      }
+    }
+    if (curr !== i) {
+      next = array[curr];
+      array[curr] = array[i];
+      array[i] = next;
+    }
+  }
+  return array;
 }
 module.exports = select;
+console.log(select([6, 18, 6, 34, 2, 6, 9, 56, 12]));
